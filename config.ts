39e816c57d8e3a81b19009466d9e590eb8fbb052
pirsonxyz <pirsonbethancourt039@gmail.com>
2024-02-10 14:05:01 -0400

    cambiar comillas dobles a comillas normales

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
-const TOKEN: string = "MTIwNTY4OTg5OTYyNjI2NjY0NA.GyF6AZ.A2-cG6rN4-b7ow1Yg5WAMFT-4hdLD1b3xAsJJ4";
-const CLIENT_ID: string = "1205689899626266644"
+const TOKEN: string = 'MTIwNTY4OTg5OTYyNjI2NjY0NA.GyF6AZ.A2-cG6rN4-b7ow1Yg5WAMFT-4hdLD1b3xAsJJ4';
+const CLIENT_ID: string = '1205689899626266644'
 if (!TOKEN || !CLIENT_ID) {
-  throw new Error("MISSING TOKEN AND CLIENT ID");
+  throw new Error('MISSING TOKEN AND CLIENT ID');
 }
 
 export const config = {
